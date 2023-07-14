eb9b2395d78f34eb30b33464237dc949a2986860
arturhonores <luis.arturhonores@gmail.com>
2023-07-14 21:14:03 +0200

    last changes

--- main.js
+++ main.js
@@ -13,32 +13,3 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
-
-
-
-// //menú responsive
-// const menuMobile = document.querySelector('.menu-mobile');
-
-
-// //hamburger button animation
-// const menuBtn = document.querySelector('.menu-btn');
-// let menuOpen = false;
-
-// menuBtn.addEventListener('click', () => {
-//     if (!menuOpen) {
-//         menuBtn.classList.add('open');
-//         // menuMobile.classList.add('h-full')
-//         menuMobile.classList.remove('top-[-100%]')
-//         menuOpen = true;
-
-//     } else {
-//         menuBtn.classList.remove('open');
-//         // menuMobile.classList.remove('h-full')
-//         menuMobile.classList.add('top-[-100%]')
-//         menuOpen = false;
-//     }
-// })
-
-// menuBtn.addEventListener('click', () => {
-//     menuMobile.classList.toggle('transition-all');
-// });
